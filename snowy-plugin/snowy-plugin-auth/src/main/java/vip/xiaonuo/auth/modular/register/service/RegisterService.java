6885cce033b7742206b4db7a20c1d4a510cd297b
DT-1997 <hx3026678597@163.com>
2025-04-28 21:15:18 +0800

    Sprint 4 - backend

--- snowy-plugin/snowy-plugin-auth/src/main/java/vip/xiaonuo/auth/modular/register/service/RegisterService.java
+++
@@ -1,9 +0,0 @@
-package vip.xiaonuo.auth.modular.register.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import vip.xiaonuo.auth.modular.register.param.RegisterParam;
-import vip.xiaonuo.client.modular.user.entity.ClientUser;
-
-public interface RegisterService extends IService<ClientUser> {
-    void doRegister(RegisterParam registerParam);
-}
